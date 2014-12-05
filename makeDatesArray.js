313cb7898607dd0713cc3552989c519667695466
Adam Pearce <1wheel@gmail.com>
2014-12-04 23:49:30 -0500

    generating list of dates

---
+++ makeDatesArray.js
@@ -0,0 +1,21 @@
+var fs = require('fs')
+var d3 = require('d3')
+var request = require('request')
+var cheerio = require('cheerio')
+var _ = require('underscore')
+var f = require('f')
+
+var dateFmt = d3.time.format('month=%m&day=%d&year=%Y')
+
+var dates = require('dates.json')
+
+var dateStrs = dates.map(f('str'))
+
+d3.time.days(dateFmt.parse('month=7&day=1&year=1985'), new Date(), 1).forEach(function(d){
+	if (!_.contains(dateStrs, dateFmt(d))){
+		dates.push({str: dateFmt})
+	}
+})
+
+
+ fs.writeFile('dates.json', JSON.stringify(dates, null, 4))
