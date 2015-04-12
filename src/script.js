754c598e1519bf160fc2f3cfa9eaae933f77bf54
Adam Pearce <1wheel@gmail.com>
2015-04-11 23:52:10 -0400

    setting up graphs

--- src/script.js
+++ src/script.js
@@ -11,4 +11,11 @@ d3.csv('../boxes.csv', function(res){
   byPlayers = d3.nest().key(f('url')).entries(data)
 
   byPlayers = _.sortBy(byPlayers, f('values', 'length'))
+
+
+
+  !(function(){
+
+  })()
+
 })
